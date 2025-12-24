65ec729033d731de6572f7a65ac56f03dc9a1c8a
kamal9205 <kr920519@gmail.com>
2025-12-24 16:05:18 +0530

    adding db connection directly in index.js file src folder

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
+dotenv.config();
+
+(async ()=> {
+   try { 
+    const connectDB = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERRR: ",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>console.log(`App is listening on port ${process.env.PORT}`));
+    }
+    catch(error){
+        console.error("MongoDB Connection FAILED: ",error)
+        throw error
+    }
+})()
